0e375c62fec014bb9cb195be3f07e86f68c44b7c
sayan047 <sayanghosh004@gmail.com>
2020-05-23 23:11:04 +0530

    changed angelbully to bully

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ client.once('ready', () => {
     console.log('Bot is Online!');
 })
 
+//welcome message
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(channel => channel.name === 'chill-zone' || channel.name === 'general');
+    if (!channel) return;
+    channel.send(`Welcome to the server, ${member}`);
+  });
+
+//commands
 client.on('message', message => {
     if (message.content.startsWith(`${prefix}greet`) && !message.author.bot) {
         let member = message.mentions.members.first();
         message.channel.send(`Hello ${member.displayName}!`);
     }
-    if (message.content.startsWith(`${prefix}angelbully`) && !message.author.bot) {
+    if (message.content.startsWith(`${prefix}bully`) && !message.author.bot) {
         let member = message.mentions.members.first();
         const exampleEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
-            .setTitle(`Angel bullies ${member.displayName}`)
+            .setTitle(`${message.author.username} bullies ${member.displayName} :punch: `)
             .setImage('https://media.giphy.com/media/jxETRYAi2KReel7pqy/giphy.gif')
         message.channel.send(exampleEmbed);
     }
