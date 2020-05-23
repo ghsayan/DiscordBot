e540ac8cef98b17e2233655f030d04c367e95a21
sayan047 <sayanghosh004@gmail.com>
2020-05-23 11:22:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const { prefix,token }=require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () =>{
+    console.log('Ready!');
+})
+
+client.on('message',message => {
+    if(message.content.startsWith(`${prefix}greet`) || !message.author.bot){
+        let member=message.mentions.members.first();
+        message.channel.send(`Hello ${member.displayName}!`);
+    }
+});
+client.login(process.env.token);
