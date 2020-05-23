7ffc997deaa70db47f87faa88c0f51a1ab5ed4b5
sayan047 <sayanghosh004@gmail.com>
2020-05-23 13:26:11 +0530

    removed token

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
-const { prefix,token }=require('./config.json');
+const {
+    prefix
+} = require('./config.json');
 const client = new Discord.Client();
 
-client.once('ready', () =>{
+client.once('ready', () => {
     console.log('Ready!');
 })
 
-client.on('message',message => {
-    if(message.content.startsWith(`${prefix}greet`) || !message.author.bot){
-        let member=message.mentions.members.first();
-        message.channel.send(`Hello ${member.displayName}!`);
-    }
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    let member = message.mentions.members.first();
+    message.channel.send(`Hello ${member.displayName}!`);
+
 });
 client.login(process.env.token);
